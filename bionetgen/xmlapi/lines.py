1c4930cd38da0685bfe66ff63fba53957d42f90a
Ali Sinan Saglam <asinansaglam@gmail.com>
2021-05-12 14:48:45 -0400

    starting xml refactoring

---
+++ bionetgen/xmlapi/lines.py
@@ -0,0 +1,19 @@
+class BaseLine:
+    '''
+    Base line object
+
+    Attributes
+    ----------
+
+
+    Methods
+    -------
+    '''
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return self.gen_string()
+
+    def __str__(self):
+        return self.gen_string()
