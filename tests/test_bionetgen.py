da40af61ba5dfc67a55194734823cfae56cae4ac
cjarmstrong97 <cjarmstrong97@gmail.com>
2021-09-03 12:02:44 -0400

    Trying to test run all models

--- tests/test_bionetgen.py
+++ tests/test_bionetgen.py
@@ -7,7 +7,7 @@ tfold = os.path.dirname(__file__)
 
 
 def test_bionetgen_help():
-    # test basic command help
+    # tests basic command help
     with raises(SystemExit):
         argv = ["--help"]
         with BioNetGenTest(argv=argv) as app:
@@ -16,7 +16,6 @@ def test_bionetgen_help():
 
 
 def test_bionetgen_input():
-    # test basic command help
     argv = [
         "run",
         "-i",
@@ -32,13 +31,14 @@ def test_bionetgen_input():
         assert file_list.sort() == to_match.sort()
 
 
-# def test_bionetgen_model():
-#     fpath = os.path.join(tfold, "test.bngl")
-#     fpath = os.path.abspath(fpath)
-#     m = bng.bngmodel(fpath)
+def test_bionetgen_model():
+    fpath = os.path.join(tfold, "test.bngl")
+    fpath = os.path.abspath(fpath)
+    m = bng.bngmodel(fpath)
 
 
 def test_bionetgen_all_model_loading():
+    # tests library model loading using many models
     mpattern = os.path.join(tfold, "models") + os.sep + "*.bngl"
     models = glob.glob(mpattern)
     succ = []
@@ -56,20 +56,21 @@ def test_bionetgen_all_model_loading():
             fails += 1
             fail.append(model)
     print("succ: {}".format(success))
-    # print(sorted(succ))
+    print(sorted(succ))
     print("fail: {}".format(fails))
     print(sorted(fail))
     assert fails == 0
 
 
 def test_action_loading():
+    # tests a BNGL file containing all BNG actions
     all_action_model = os.path.join(*[tfold, "models", "all_actions.bngl"])
     m = bng.bngmodel(all_action_model)
     assert len(m.actions) == 27
 
 
 def test_bionetgen_info():
-    # test info subcommand
+    # tests info subcommand
     argv = ["info"]
     with BioNetGenTest(argv=argv) as app:
         app.run()
@@ -77,29 +78,29 @@ def test_bionetgen_info():
 
 
 def test_model_running_CLI():
-    # test running a few models using the CLI
-    models = ["test_MM.bngl", "motor.bngl", "simple_system.bngl"]
+    # tests running a list of models using the CLI
+    mpattern = os.path.join(tfold, "models") + os.sep +"*.bngl"
+    models = glob.glob(mpattern)
     succ = []
     fail = []
     success = 0
     fails = 0
     for model in models:
-        fpath = os.path.join(*[tfold, "models", model])
-        fpath = os.path.abspath(fpath)
         try:
-            fpath = os.path.join(*[tfold, "models", model])
-            fpath = os.path.abspath(fpath)
-            argv = ["run", "-i", fpath, "-o", "cli_test_runs"]
+            argv = ["run", "-i", model, "-o", "cli_test_runs"]
             with BioNetGenTest(argv=argv) as app:
                 app.run()
                 assert app.exit_code == 0
             success += 1
+            model = os.path.split(model)
+            model = model[1]
             succ.append(model)
         except:
             print("can't run model {}".format(model))
             fails += 1
+            model = os.path.split(model)
+            model = model[1]
             fail.append(model)
-    del model, fpath
     print("succ: {}".format(success))
     print(sorted(succ))
     print("fail: {}".format(fails))
@@ -108,24 +109,26 @@ def test_model_running_CLI():
 
 
 def test_model_running_lib():
-    # test running a few models using the library
-    models = ["test_MM.bngl", "motor.bngl", "simple_system.bngl"]
+    # test running a list of models using the library
+    mpattern = os.path.join(tfold, "models") + os.sep + "*.bngl"
+    models = glob.glob(mpattern)
     succ = []
     fail = []
     success = 0
     fails = 0
     for model in models:
-        fpath = os.path.join(*[tfold, "models", model])
-        fpath = os.path.abspath(fpath)
         try:
-            bng.run(fpath)
+            bng.run(model)
             success += 1
+            model = os.path.split(model)
+            model = model[1]
             succ.append(model)
         except:
             print("can't run model {}".format(model))
             fails += 1
+            model = os.path.split(model)
+            model = model[1]
             fail.append(model)
-    del model, fpath
     print("succ: {}".format(success))
     print(sorted(succ))
     print("fail: {}".format(fails))
