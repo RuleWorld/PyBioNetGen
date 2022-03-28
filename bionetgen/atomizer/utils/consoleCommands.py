48b5cf64373fb69df06c2f6c06fb73f605febb4c
ASinanSaglam <asinansaglam@gmail.com>
2022-03-28 12:00:54 -0400

    fixing the usage of `write_xml` method

--- bionetgen/atomizer/utils/consoleCommands.py
+++ bionetgen/atomizer/utils/consoleCommands.py
@@ -4,16 +4,7 @@ Created on Mon Sep  2 18:11:35 2013
 
 @author: proto
 """
-
-import platform
-
 import bionetgen
-import subprocess
-import os
-
-from os.path import expanduser, join
-
-home = expanduser("~")
 
 
 def setBngExecutable(executable):
@@ -27,5 +18,7 @@ def getBngExecutable():
 
 def bngl2xml(bnglFile, timeout=60):
     mdl = bionetgen.modelapi.bngmodel(bnglFile)
-    mdl.bngparser.bngfile.write_xml(bnglFile, xml_type="bngxml", bngl_str=str(mdl))
+    xml_file = bnglFile.replace(".bngl", ".xml")
+    with open(xml_file, "w+") as f:
+        mdl.bngparser.bngfile.write_xml(f, xml_type="bngxml", bngl_str=str(mdl))
     # TODO: Deal with timeout here
